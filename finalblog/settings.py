20f7ae5cc9d4207aabdb1d6b35b8f766e85a4475
Ivan Yeliseyev <vano.eliseev@mail.ru>
2021-09-23 19:04:05 +0300

    Path2

--- finalblog/settings.py
+++ finalblog/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/

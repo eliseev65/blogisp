bcd591e5b366bf19cdc7b3bec7310ba73e0ff36c
Ivan Yeliseyev <vano.eliseev@mail.ru>
2021-09-23 19:22:02 +0300

    Path3

--- finalblog/settings.py
+++ finalblog/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles"),
+
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
